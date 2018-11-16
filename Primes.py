c6aa94d15aa05dc008e1547842b8925caef4f706
Rod Lewis <rodlewis360@gmail.com>
2018-11-15 19:02:17 -0800

    initial project version

---
+++ Primes.py
@@ -0,0 +1,34 @@
+import math
+
+
+def isprime(p):
+    mod = 2
+    sqrt_prime = math.sqrt(p)
+    while not (mod > sqrt_prime):
+        if ((p % mod) == 0):
+            Prime = 0
+            p += 1
+            return p
+            break
+        else:
+            mod += 1
+
+    Prime = 1
+    return Prime
+
+
+def primeloop(p):
+    Prime = 0
+    while not (Prime == 1):
+        p = isprime(p)
+
+    print("Your prime is", p)
+
+
+def ask():
+    prime = int(
+        input("What number?                                          "))
+    primeloop(prime)
+
+
+ask()
