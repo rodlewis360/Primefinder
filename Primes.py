6f277ee565261d0d9729a6a51151ee1eb912bb70
Rod Lewis <rodlewis360@gmail.com>
2018-11-27 21:45:48 -0800

    Added Rabin Miller

--- Primes.py
+++ Primes.py
@@ -5,28 +5,43 @@ def isprime(p):
     mod = 2
     sqrt_prime = math.sqrt(p)
     while not (mod > sqrt_prime):
+        nPrime = 0
         if ((p % mod) == 0):
             Prime = 0
             p += 1
-            return p
+            nPrime = 1
             break
         else:
             mod += 1
 
     Prime = 1
-    return Prime
+<<<<<<< HEAD
+    return nPrime, p, Prime
+=======
+    return (nPrime, p, Prime)
+>>>>>>> b4aeece33a6ddea606ba61132b3b66001fd07b9a
 
 
 def primeloop(p):
     Prime = 0
-    while not (Prime == 1):
-        p = isprime(p)
+    nPrime = 1
+<<<<<<< HEAD
+    while (nPrime == 1) or not (Prime == 1):
+        nPrime = 0
+        print("calculating", p)
+=======
+    while (nPrime == 1 ) or not (Prime == 1):
+        nPrime = 0
+>>>>>>> b4aeece33a6ddea606ba61132b3b66001fd07b9a
+        q = isprime(p)
+        nPrime, p, Prime = q
 
     print("Your prime is", p)
 
 
 def ask():
     prime = int(input("What number?                                      "))
+    print("calcuating")
     primeloop(prime)
 
 
