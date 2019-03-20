c3d68b55424c100474f775c54f93b19461d9fbb6
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-03-20 15:15:52 -0700

    Made something...  We'll see if it works.

--- Primefinder3.py
+++ Primefinder3.py
@@ -1,1 +1,18 @@
+def fib(n):
+    a, b = 0, 1
+    lst = [0, 1]
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+        lst.append(b)
+    print()
+    return lst
 
+def primefinder():
+    print("How high?")
+    fibonacci = fib(input())
+    for a in fibonacci:
+        lst.append(6 * a)
+    for a in lst:
+        print(a)
+        
